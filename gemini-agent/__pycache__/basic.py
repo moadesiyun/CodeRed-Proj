40e7cbb4ec51265c45354fb73d73cb72118b4b6e
Mo Adesiyun <8804eply.github.com3947+moadesiyun@usersensions.noreply.github.com>
2024-02-10 18:36:39 -0600

    Co-authored-by: Happydragon123 <Happydragon123@users.noreply.github.com> Co-authored-by: Donaldo Almazan <donaldoalmazan@users.noreply.github.com>

---
+++ gemini-agent/__pycache__/basic.py
@@ -0,0 +1,49 @@
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyCdU5Lt7WN-SrF3Rpx0OX4FDCF5PKhJxD4")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+prompt_parts = [
+  "Create Json of flight data based on a sentence. Fields should include \"departure_airport\", \"arrival_airport\", \"departure_date\",\"budget\". Date should be formatted as MM/DD/YYYY.\n",
+]
+
+line = input()
+prompt_parts += line 
+response = model.generate_content(prompt_parts)
+print(response.text)
